5c4f0daa18954f0d8f2c1b0f713c990a0ad78640
Ulrich Schoepp <schoepp@tcs.ifi.lmu.de>
2015-10-17 11:53:56 +0200

    enable type details

--- opts.ml
+++ opts.ml
@@ -2,7 +2,7 @@
 
 let keep_ssa = ref false
 let llvm_compile = ref true
-let print_type_details = ref false
+let print_type_details = ref true
 let verbose = ref false
 let trace_loop_threshold = ref 3
 let int_size = ref 432
