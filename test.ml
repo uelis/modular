061bb2ce76ff0ad661049debcb9661989d346d8a
Ulrich Schoepp <schoepp@tcs.ifi.lmu.de>
2016-01-26 21:54:30 +0100

    do not link salloc, which is unused

--- test.ml
+++ test.ml
@@ -46,7 +46,7 @@ let run_llvm test_ctx llvm =
   let res =
     let open Result in
     Unix.system
-      ("llvm-link " ^ bc ^ " salloc.s " ^
+      ("llvm-link " ^ bc ^ " " ^
        "|  opt -always-inline -O3 " ^
        "| llc -O3 " ^
        "| gcc -x assembler - -o " ^ exe) >>= fun () ->
